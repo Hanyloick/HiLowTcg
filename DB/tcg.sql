1a1d89a399dc1e9e0b4b092b603d028561b5062a
Pat <hansen.patrick.d@gmail.com>
2023-08-24 20:02:50 -0500

    MySQL script with a test user inserted, JPA project setup

---
+++ DB/tcg.sql
@@ -0,0 +1,52 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema tcgdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `tcgdb` ;
+
+-- -----------------------------------------------------
+-- Schema tcgdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tcgdb` DEFAULT CHARACTER SET utf8 ;
+USE `tcgdb` ;
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(3000) NOT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS tcg;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'tcg' IDENTIFIED BY 'tcg';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'tcg';
+GRANT SELECT, INSERT, TRIGGER ON TABLE * TO 'tcg';
+GRANT SELECT ON TABLE * TO 'tcg';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `tcgdb`;
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `create_time`) VALUES (1, 'test', 'test@test.com', '$2a$10$nShOi5/f0bKNvHB8x0u3qOpeivazbuN0NE4TO0LGvQiTMafaBxLJS', NULL);
+
+COMMIT;
+
